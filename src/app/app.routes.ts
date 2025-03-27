1f091b5cb34e5c44ff89446ff43cf88180a75491
Jakub Zak <j.zak19@unimail.winchester.ac.uk>
2025-03-27 11:09:12 +0000

    Added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '', component: LoginpageComponent},
+    {path:'main', component: MainpageComponent}
+];
